8ec9358312b57829236daa5ca32d3019cac1abb7
Mark Boszko <mark@boszko.com>
2015-05-13 16:31:22 -0700

    improved formatting

--- intertial-bounce.js
+++ intertial-bounce.js
@@ -14,9 +14,9 @@ freq = 3.0;
 decay = 10.0;
 
 n = 0;
-if (numKeys > 0) {
-     n = nearestKey(time).index;
-     if (key(n).time > time) {
+if ( numKeys > 0 ) {
+     n = nearestKey( time ).index;
+     if ( key(n).time > time ) {
           n--;
      }
 }
@@ -26,8 +26,8 @@ if (n == 0) {
      t = time - key(n).time;
 }
 if (n > 0 && t < 1) {
-     v = velocityAtTime(key(n).time - thisComp.frameDuration / 10);
-     value + v*amp*Math.sin(freq*t*2*Math.PI)/Math.exp(decay*t);
+     v = velocityAtTime( key(n).time - thisComp.frameDuration / 10 );
+     value + v * amp * Math.sin( freq * t * 2 * Math.PI ) / Math.exp( decay * t );
 } else {
      value
 }
