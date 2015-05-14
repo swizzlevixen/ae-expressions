43c15d1714c417b90798794279a6e1ef06c4d47b
Mark Boszko <mark@boszko.com>
2015-05-13 17:44:06 -0700

    formatting improvements

--- scale-with-bounce.js
+++ scale-with-bounce.js
@@ -1,26 +1,30 @@
-// Honestly, I'm not sure why you would use this instead of intertial-bounce,
-// but it was in my archives, so here it is.
+// Scale with Bounce
+// - Honestly, I'm not sure why you would use this instead of intertial-bounce,
+//   but it was in my archives, so here it is.
+//
 // Source: unknown
+//
+// Usage: Umm… fiddle with it?
 
 rate = 200;
-rampDur = value[0]/rate;
+rampDur = value[0] / rate;
 freq = 3;
 decay = 15;
-w = freq*Math.PI*2;
+w = freq * Math.PI * 2;
 
-if (time < (inPoint+outPoint)/2){
-  if (time < (inPoint + rampDur)){
-    s = linear(time,inPoint,inPoint+rampDur,0,value[0]);
-  }else{
-    t = time - (inPoint+rampDur);
-    s = value[0] + rate*Math.sin(t*w)/Math.exp(t*decay)/w;
-  }
-}else{
-  if (time > (outPoint - rampDur)){
-    s = linear(time,outPoint-rampDur,outPoint,value[0],0);
-  }else{
-    t = (outPoint - rampDur) - time;
-    s = value[0] + rate*Math.sin(t*w)/Math.exp(t*decay)/w;
-  }
+if ( time < ( inPoint + outPoint ) / 2 ) {
+    if ( time < ( inPoint + rampDur ) ) {
+        s = linear( time, inPoint, inPoint + rampDur, 0, value[0] );
+    } else {
+        t = time - ( inPoint + rampDur );
+        s = value[0] + rate * Math.sin( t * w ) / Math.exp( t * decay ) / w;
+    }
+} else {
+    if ( time > ( outPoint - rampDur ) ) {
+        s = linear( time, outPoint - rampDur, outPoint, value[0], 0);
+    } else {
+        t = ( outPoint - rampDur ) - time;
+        s = value[0] + rate * Math.sin( t * w ) / Math.exp( t * decay ) / w;
+    }
 }
-[s,s]
+[s,s] // for locked scale
