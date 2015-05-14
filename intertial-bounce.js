fd3223e8ef5561c62996ab8f9b60008dd6b0baf0
Mark Boszko <mark@boszko.com>
2015-05-13 17:38:54 -0700

    updated source

--- intertial-bounce.js
+++ intertial-bounce.js
@@ -1,10 +1,10 @@
-// Inertial Bounce (moves settle into place after bouncing around a little)
-
-// I feel like this is probably based on something from MotionScript.com, but
-// this is the closest one I see online:
+// Inertial Bounce
+// - moves settle into place after bouncing around a little
+//
+// Source:
 // http://www.graymachine.com/my-top-5-after-effects-expressions/
-
-// Tweak these settings to customize animation:
+//
+// Usage:
 // amp: amplitude, how much bounce is present
 // freq: frequency, how frequently the bounce occurs
 // decay: a friction/mass setting, higher value means a shorter decay
@@ -15,19 +15,19 @@ decay = 10.0;
 
 n = 0;
 if ( numKeys > 0 ) {
-     n = nearestKey( time ).index;
-     if ( key(n).time > time ) {
-          n--;
-     }
+    n = nearestKey( time ).index;
+    if ( key(n).time > time ) {
+        n--;
+    }
 }
 if (n == 0) {
-     t = 0;
+    t = 0;
 } else {
-     t = time - key(n).time;
+    t = time - key(n).time;
 }
 if (n > 0 && t < 1) {
-     v = velocityAtTime( key(n).time - thisComp.frameDuration / 10 );
-     value + v * amp * Math.sin( freq * t * 2 * Math.PI ) / Math.exp( decay * t );
+    v = velocityAtTime( key(n).time - thisComp.frameDuration / 10 );
+    value + v * amp * Math.sin( freq * t * 2 * Math.PI ) / Math.exp( decay * t );
 } else {
-     value
+    value
 }
